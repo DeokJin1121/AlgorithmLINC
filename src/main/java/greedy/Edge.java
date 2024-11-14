e7ba578a78b1b321a74129a96c9bc8af1f38af05
byeondeokjin <ejrwls1463@naver.com>
2024-11-14 14:14:01 +0900

    11.14 Algorithm_greedy
    11.14 Algorithm_greedy

---
+++ src/main/java/greedy/Edge.java
@@ -0,0 +1,16 @@
+package greedy;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Edge {
+    private int v1;
+    private int v2;
+    private int weight;
+}
