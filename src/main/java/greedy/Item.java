a49a6807f8cbc6a668d2f281887588e423282efe
byeondeokjin <ejrwls1463@naver.com>
2024-11-21 13:31:53 +0900

    2024.11.21 Algorithm
    Knapsack

---
+++ src/main/java/greedy/Item.java
@@ -0,0 +1,16 @@
+package greedy;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private int weight;
+    private int value;
+    private double unitValue;
+
+    public Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+        this.unitValue = value/weight;
+    }
+}
---
+++ src/main/java/greedy/Knapsack.java
@@ -0,0 +1,63 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Knapsack {
+    private int[] unitValue;
+
+    private void calValue(int[] weight, int[] value, int W) {
+        unitValue = new int[value.length];
+
+        for (int i = 0; i < value.length; i++) {
+            unitValue[i] = value[i] / weight[i];
+        }
+        int cW = 0;    // 물건의 무게 합
+        int cV = 0;    // 물건의 가치 합
+
+        int x = findMax();
+
+        while((cW + weight[x] < W)) {
+            cW = cW + weight[x];
+            cV = cV + value[x];
+            unitValue[x] = -1;
+            x = findMax();
+        }
+        System.out.println("총합 : " + cW + ", 총 가치 : " + cV);
+
+        if((W - cW) > 0) {
+            cV = cV + (W - cW) * unitValue[x];
+        }
+        System.out.println("총 가치 : " + cV);
+    }
+
+    private int findMax() {
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for (int i = 0 ; i < unitValue.length; i++) {
+            if(unitValue[i] > max) {
+                max = unitValue[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
+    public static void main(String[] args) {
+        Knapsack knapsack = new Knapsack();
+
+        int[] weight = {5, 10, 20};
+        int[] value = {50, 60, 140};
+        int capacity = 30;
+        
+        Item item1 = new Item(5,50);
+        Item item2 = new Item(10,60);
+        Item item3 = new Item(20,140);
+
+        List<Item> knapsackItems = Arrays.asList(item1, item2, item3);
+
+        knapsack.calValue(weight, value, capacity);
+    }
+}
